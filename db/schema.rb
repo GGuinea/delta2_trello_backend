6a075f0b24ab21f18ee02428aa80ab4f3f9ece72
Kamil_Zielinski <215933@edu.p.lodz.pl>
2020-11-23 20:15:27 +0100

    column name fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_07_151218) do
+ActiveRecord::Schema.define(version: 2020_11_23_190634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2020_11_07_151218) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.text "descripton"
+    t.text "description"
   end
 
   create_table "members", id: false, force: :cascade do |t|
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 2020_11_07_151218) do
     t.string "password"
     t.string "username"
     t.string "email"
+    t.string "activation_digest"
+    t.boolean "activated", default: false
   end
 
 end
