aea082a14a0bc54f429ed006e446d2fe67d25ae0
Kamil_Zielinski <215933@edu.p.lodz.pl>
2020-12-10 12:35:59 +0100

    Added validation for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-  validates :username, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true, length: { in: 5..20, 
+          wrong_length: "{count} characters is not allowed for username" }
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true
+  validates :password, presence: true, length: { in: 6..20,
+          wrong_length: "{count} characters is not allowed for password"}
   has_and_belongs_to_many :boards, :join_table => :members
 end
